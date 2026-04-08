59ce6b14def397ee2c833259ceb8980742783193
Fernando Guerreiro <fernando.rodrigues@prodap.com.br>
2026-04-08 13:09:00 -0300

    style: format code for consistency in useAuth hook

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -2,11 +2,11 @@ import { useContext } from "react";
 import { AuthContext } from "../auth/context/auth-context";
 
 export function useAuth() {
-  const context = useContext(AuthContext);
+	const context = useContext(AuthContext);
 
-  if (!context) {
-    throw new Error("useAuth must be used within AuthProvider.");
-  }
+	if (!context) {
+		throw new Error("useAuth must be used within AuthProvider.");
+	}
 
-  return context;
+	return context;
 }
